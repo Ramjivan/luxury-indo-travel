f42c0c73661f319465f3ce933a5c30c35917c536
Ramjian Jangid <ramjeewanj@gmail.com>
2018-11-13 14:21:52 +0530

    ontact page

---
+++ js/custom.js
@@ -0,0 +1,6 @@
+//load social media icon links
+
+$("#fb").attr("href", "http://www.facebook.com/")
+$("#twitter").attr("href", "#")
+$("#insta").attr("href", "http://www.instagram.com/")
+$("#g-plus").attr("href", "#")
